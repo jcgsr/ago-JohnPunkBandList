9dfa9a0e1cf6434c6ee237028045363cf8d40d3c
jcgsr <cabralguerra@gmail.com>
2021-08-17 18:06:50 -0300

    search struggle continues

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,67 @@
+// import React, { useState, useEffect } from "react";
+
+// import api from "../assets/services/api";
+
+// const SearchBar = () => {
+//   const [searchBand, setSearchBand] = useState("");
+//   const [bands, setBands] = useState([]);
+//   useEffect(() => {
+//     const loadBands = async () => {
+//       const response = await api.get();
+//       setBands(response.data);
+//       console.log(response.data);
+//     };
+//     loadBands();
+//   }, []);
+//   return (
+//     <section>
+//       <form>
+//         <input
+//           type="text"
+//           placeholder="buscar bandas"
+//           autoFocus
+//           value={searchBand}
+//           aria-label="Search"
+//           onChange={e => setSearchBand(e.target.value)}
+//         />
+//       </form>
+//       {bands
+//         .filter(val => {
+//           if (searchBand == "") {
+//             return val;
+//           } else if (
+//             val.bands.toLowerCase().includes(searchBand.toLowerCase())
+//           ) {
+//             return val;
+//           }
+//         })
+//         .map(bands => (
+//           <article key={bands.band}>
+//             <h2>{bands.band}</h2>
+//           </article>
+//         ))}
+//     </section>
+//   );
+// };
+
+// export default SearchBar;
+
 import React from "react";
 
 const SearchBar = ({ searchQuery, setSearchQuery }) => (
   <form action="/" method="get" autoComplete="off">
     <label htmlFor="header-search">
-      <span className="visually-hidden">Buscar bandas</span>
+      <span className="visually-hidden">Buscar Bandas</span>
     </label>
     <input
-      type="text"
       value={searchQuery}
-      onInput={e => setSearchQuery(e.target.value)}
+      onChange={e => setSearchQuery(e.target.value)}
+      type="text"
       id="header-search"
       placeholder="buscar bandas"
       name="s"
-    />{" "}
-    <button type="submit">buscar</button>
+    />
+    {/* <button type="submit">Search</button>*/}
   </form>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,52 +3,70 @@
 import React, { useState } from "react";
 import { StaticQuery, graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
-
+import { useFlexSearch } from "react-use-flexsearch";
 import Layout from "../components/Layout";
 import SearchBar from "../components/Search";
 
-const John = () => (
-  <Layout>
-    <main className="container">
-      <h1>john's ultimate punk bands list</h1>
-      <SearchBar />
-      <StaticQuery
-        query={query}
-        render={data => (
-          <div className="bands">
-            {data.allStrapiBand.nodes.map(node => (
-              <article key={node.id}>
-                <h2>{node.band_name}</h2>
-                <GatsbyImage
-                  image={
-                    node.band_img.localFile.childImageSharp.gatsbyImageData
-                  }
-                  alt={node.band_name}
-                />
-                <p>{node.band_desc}</p>
-                <div className="btns">
-                  <a href={node.band_site} target="_blank">
-                    <button>site oficial</button>
-                  </a>
-                  <a href={node.youtube} target="_blank">
-                    <button>youtube</button>
-                  </a>
-                  <a href={node.wikipedia} target="_blank">
-                    <button>wikipedia</button>
-                  </a>
-                </div>
-                <hr />
-              </article>
-            ))}
-          </div>
-        )}
-      />
-    </main>
-  </Layout>
-);
+const John = ({
+  bandData: {
+    localSearchBands: { index, store },
+  },
+}) => {
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get("s");
+  const [searchQuery, setSearchQuery] = useState(query || "");
+  const results = useFlexSearch(searchQuery, index, store);
+  return (
+    <Layout>
+      <main className="container">
+        <h1>john's ultimate punk band list</h1>
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <p>{searchQuery}</p>
+        {console.log(searchQuery)}
+        <StaticQuery
+          query={bandQuery}
+          render={data => (
+            <div className="bands">
+              {data.allStrapiBand.nodes.map(node => (
+                <article key={node.id}>
+                  <h2>{node.band_name}</h2>
+                  <GatsbyImage
+                    image={
+                      node.band_img.localFile.childImageSharp.gatsbyImageData
+                    }
+                    alt={node.band_name}
+                  />
+                  <p>{node.band_desc}</p>
+                  <div className="btns">
+                    <a href={node.band_site} target="_blank">
+                      <button>site oficial</button>
+                    </a>
+                    <a href={node.youtube} target="_blank">
+                      <button>youtube</button>
+                    </a>
+                    <a href={node.wikipedia} target="_blank">
+                      <button>wikipedia</button>
+                    </a>
+                  </div>
+                  <hr />
+                </article>
+              ))}
+            </div>
+          )}
+        />
+      </main>
+    </Layout>
+  );
+};
 
-const query = graphql`
+const bandQuery = graphql`
   {
+    localSearchBands {
+      index
+      store
+      publicIndexURL
+      publicStoreURL
+    }
     allStrapiBand {
       nodes {
         band_img {
