d8ccab4f055cb903ea94a50679fffea9148c302d
jcgsr <cabralguerra@gmail.com>
2021-08-30 22:18:30 -0300

    adsense

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} - John's Ultimate Punk Band List</p>
+      <p>&copy; {new Date().getFullYear()} - PunkCore</p>
       <p>Todos os direitos reservados, Cabrón!</p>
     </footer>
   );
